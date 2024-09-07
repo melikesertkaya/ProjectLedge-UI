400a6786eb766a758c5de5eda2e2af022b32c072
melike sertkaya <melikesertkaya99@gmail.com>
2024-09-07 15:08:00 +0300

    fx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { CompanyComponent } from './component/company/company.component';
 import { EmployeeComponent } from './component/employee/employee.component';
 import { EmployeeAddComponent } from './component/employee/employee-add/employee-add.component';
 import { EmployeeEditComponent } from './component/employee/employee-edit/employee-edit.component';
+import { EmployeeDeleteComponent } from './component/employee/employee-delete/employee-delete.component';
 
 const routes: Routes = [
   {path: '', component: PrincipalComponent},
   {path: 'example/company', component: CompanyComponent},
   {path: 'example/employee', component: EmployeeComponent},
-  {path: 'example/employeeAdd', component: EmployeeAddComponent},
-  {path: 'example/employeeEdit/:id', component: EmployeeEditComponent}
+  {path: 'employee/add', component: EmployeeAddComponent},
+  {path: 'employee/edit/:id', component: EmployeeEditComponent},
+  {path: 'employee/delete/:id', component: EmployeeDeleteComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
