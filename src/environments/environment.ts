9e6e00e2aee8469c4d857712be4967cb2a6c6fb3
melike sertkaya <melikesertkaya99@gmail.com>
2024-10-02 23:02:04 +0300

    fx environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment={
     production:false,
-    apiUrl:"https://localhost:44333/api/"
+    apiUrl:"https://projectledgeserviceapi20240918221855.azurewebsites.net/api/"
 }
- //apiUrl:"https://projectledgeserviceapi20240918221855.azurewebsites.net/api/"
+ //apiUrl:"https://localhost:44333/api/"
